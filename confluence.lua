c891e0392ca0e0882f32424d638bf1f0582d38f7
Al Manning <al.manning@rstudio.com>
2022-09-01 10:12:43 +0200

    headers, bold, emph

---
+++ confluence.lua
@@ -0,0 +1,100 @@
+-- Confluence Storage Format for Pandoc
+-- https://confluence.atlassian.com/doc/confluence-storage-format-790796544.html
+-- https://pandoc.org/MANUAL.html#custom-readers-and-writers
+
+-- Character escaping
+local function escape(s, in_attribute)
+  return s:gsub("[<>&\"']",
+          function(x)
+            if x == '<' then
+              return '&lt;'
+            elseif x == '>' then
+              return '&gt;'
+            elseif x == '&' then
+              return '&amp;'
+            elseif in_attribute and x == '"' then
+              return '&quot;'
+            elseif in_attribute and x == "'" then
+              return '&#39;'
+            else
+              return x
+            end
+          end)
+end
+
+-- The functions that follow render corresponding pandoc elements.
+-- s is always a string, attr is always a table of attributes, and
+-- items is always an array of strings (the items in a list).
+-- Comments indicate the types of other variables.
+
+function Str(s)
+  return escape(s)
+end
+
+function Space()
+  return " "
+end
+
+-- Table to store footnotes, so they can be included at the end.
+local notes = {}
+
+-- Blocksep is used to separate block elements.
+function Blocksep()
+  return "\n"
+end
+
+-- This function is called once for the whole document. Parameters:
+-- body is a string, metadata is a table, variables is a table.
+-- This gives you a fragment.  You could use the metadata table to
+-- fill variables in a custom lua template.  Or, pass `--template=...`
+-- to pandoc, and pandoc will do the template processing as usual.
+function Doc(body, metadata, variables)
+  local buffer = {}
+  local function add(s)
+    table.insert(buffer, s)
+  end
+  add(body)
+  if #notes > 0 then
+    add('<ol class="footnotes">')
+    for _,note in pairs(notes) do
+      add(note)
+    end
+    add('</ol>')
+  end
+  return table.concat(buffer,'\n') .. '\n'
+end
+
+function Header(level, s)
+  return "<h" .. level ..  ">" .. s .. "</h" .. level .. ">"
+end
+
+function Para(s)
+  return "<p>" .. s .. "</p>"
+end
+
+function RawInline(format, str)
+  if format == "html" then
+    return str
+  else
+    return ''
+  end
+end
+
+function Emph(s)
+  return "<em>" .. s .. "</em>"
+end
+
+function Strong(s)
+  return "<strong>" .. s .. "</strong>"
+end
+
+-- The following code will produce runtime warnings when you haven't defined
+-- all of the functions you need for the custom writer, so it's useful
+-- to include when you're working on a writer.
+local meta = {}
+meta.__index =
+function(_, key)
+  io.stderr:write(string.format("WARNING: Undefined function '%s'\n",key))
+  return function() return "" end
+end
+setmetatable(_G, meta)
