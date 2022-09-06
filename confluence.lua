0e586db1fd94aebade396b4b02280eb387ed86b2
Al Manning <al.manning@rstudio.com>
2022-09-06 10:41:27 +0200

    fix tables, no equations

--- confluence.lua
+++ confluence.lua
@@ -5,6 +5,7 @@
 local pipe = pandoc.pipe
 local stringify = (require "pandoc.utils").stringify
 local confluence = require('confluence-overrides')
+local html_align = confluence.html_align
 
 -- The global variable PANDOC_DOCUMENT contains the full AST of
 -- the document which is going to be written. It can be used to
@@ -233,20 +234,6 @@ function DefinitionList(items)
   return "<dl>\n" .. table.concat(buffer, "\n") .. "\n</dl>"
 end
 
--- Convert pandoc alignment to something HTML can use.
--- align is AlignLeft, AlignRight, AlignCenter, or AlignDefault.
-local function html_align(align)
-  if align == 'AlignLeft' then
-    return 'left'
-  elseif align == 'AlignRight' then
-    return 'right'
-  elseif align == 'AlignCenter' then
-    return 'center'
-  else
-    return 'left'
-  end
-end
-
 function CaptionedImage(src, tit, caption, attr)
   return confluence.CaptionedImageConfluence(src, tit, caption, attr)
 end
@@ -255,46 +242,7 @@ end
 -- widths is an array of floats, headers is an array of
 -- strings, rows is an array of arrays of strings.
 function Table(caption, aligns, widths, headers, rows)
-  local buffer = {}
-  local function add(s)
-    table.insert(buffer, s)
-  end
-  add("<table>")
-  if caption ~= "" then
-    add("<caption>" .. caption .. "</caption>")
-  end
-  if widths and widths[1] ~= 0 then
-    for _, w in pairs(widths) do
-      add('<col width="' .. string.format("%.0f%%", w * 100) .. '" />')
-    end
-  end
-  local header_row = {}
-  local empty_header = true
-  for i, h in pairs(headers) do
-    local align = html_align(aligns[i])
-    table.insert(header_row,'<th align="' .. align .. '">' .. h .. '</th>')
-    empty_header = empty_header and h == ""
-  end
-  if empty_header then
-    head = ""
-  else
-    add('<tr class="header">')
-    for _,h in pairs(header_row) do
-      add(h)
-    end
-    add('</tr>')
-  end
-  local class = "even"
-  for _, row in pairs(rows) do
-    class = (class == "even" and "odd") or "even"
-    add('<tr class="' .. class .. '">')
-    for i,c in pairs(row) do
-      add('<td align="' .. html_align(aligns[i]) .. '">' .. c .. '</td>')
-    end
-    add('</tr>')
-  end
-  add('</table>')
-  return table.concat(buffer,'\n')
+  return TableConfluence(caption, aligns, widths, headers, rows)
 end
 
 function RawBlock(format, str)
